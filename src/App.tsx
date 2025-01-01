17ec9a7fc1af59b0df9ed8420f7f7dd5a0d0bd52
Aguthankgod <Aguthankgod@gmail.com>
2025-01-01 12:06:54 +0100

    add dotenv, joi, and lodash dependencies; configure Tailwind CSS and update App component structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,13 @@
 import React from "react";
 import "./App.css";
+import Login from "./componenents/Auth/Login.tsx";
 
 function App() {
-  return <div className="App">heloo world</div>;
+  return (
+    <div className="App">
+      <Login />
+    </div>
+  );
 }
 
 export default App;
