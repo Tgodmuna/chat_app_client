ebcc9b504a9577e2590f870075241bcf70743293
Aguthankgod <Aguthankgod@gmail.com>
2025-01-07 23:50:22 +0100

    add routing support to App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import "./App.css";
 import Login from "./componenents/Auth/Login.tsx";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
