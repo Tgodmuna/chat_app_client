680c2685206b14fee17e2c5e6ce3d635f19f4e29
Aguthankgod <Aguthankgod@gmail.com>
2025-01-11 22:18:35 +0100

    refactor ChatBG component to use TypeScript for better type safety

--- src/componenents/pages/ChatBackground.tsx
+++ src/componenents/pages/ChatBackground.tsx
@@ -13,11 +13,14 @@ import { FaRegComments } from "react-icons/fa6";
  *
  * @returns {JSX.Element} A div containing a centered icon and message.
  */
-const ChatBG = () => {
+const ChatBG: React.FC = (): JSX.Element => {
   return (
     <div className="flex items-center justify-center h-[100vh] w-full bg-gray-200">
       <div className="text-center flex flex-col items-center justify-center text-wrap">
-        <FaRegComments size={100} className=" text-gray-400 mb-4" />
+        <FaRegComments
+          size={100}
+          className=" text-gray-400 mb-4"
+        />
         <p className="text-gray-500 italic">Select a chat to read messages</p>
       </div>
     </div>
