f5331673fea99cb8615e7b5fdfba7c41b7d4aa3a
Aguthankgod <Aguthankgod@gmail.com>
2025-01-11 16:07:49 +0100

    add ChatBG component for chat application background

---
+++ src/componenents/pages/ChatBackground.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { FaRegComments } from "react-icons/fa6";
+
+/**
+ * ChatBG component renders a background for the chat application.
+ * It displays a centered message prompting the user to select a chat to read messages.
+ *
+ * @component
+ * @example
+ * return (
+ *   <ChatBG />
+ * )
+ *
+ * @returns {JSX.Element} A div containing a centered icon and message.
+ */
+const ChatBG = () => {
+  return (
+    <div className="flex items-center justify-center h-[100vh] w-full bg-gray-200">
+      <div className="text-center flex flex-col items-center justify-center text-wrap">
+        <FaRegComments size={100} className=" text-gray-400 mb-4" />
+        <p className="text-gray-500 italic">Select a chat to read messages</p>
+      </div>
+    </div>
+  );
+};
+
+export default ChatBG;
