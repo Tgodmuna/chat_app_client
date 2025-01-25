fda7872e4757d795a8be8eb89d71d92386be4ad6
Aguthankgod <Aguthankgod@gmail.com>
2025-01-25 12:51:05 +0100

    add FriendList component to Layout and update context for friend visibility

--- src/componenents/pages/chats_page/Layout.tsx
+++ src/componenents/pages/chats_page/Layout.tsx
@@ -3,19 +3,22 @@ import SideBar from "./SideBar.tsx";
 import ChatBG from "./ChatBackground.tsx";
 import Chats from "./Chats.tsx";
 import type { LayoutContextType } from "../../../types.tsx";
+import FriendList from "../../user/FriendList.tsx";
 
 export const LayoutContext = React.createContext<null | LayoutContextType>(null);
 const Layout = () => {
   const [showChats, setShowChats] = useState<boolean>(false);
+  const [showFriends, setShowFriends] = useState<boolean>(false);
 
-  // layout context
   return (
-    <LayoutContext.Provider value={{ showChats, setShowChats }}>
+    <LayoutContext.Provider value={{ showChats, setShowChats, showFriends, setShowFriends }}>
       <div className={`flex w-full overflow max-w-full h-[100vw]`}>
         <SideBar />
         <ChatBG />
+
         {/* other pages */}
         <Chats />
+        <FriendList />
       </div>
     </LayoutContext.Provider>
   );
