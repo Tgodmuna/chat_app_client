86c57e7778a0dc2103c808f4db53c21c2b9e833e
Aguthankgod <Aguthankgod@gmail.com>
2025-03-04 16:52:54 +0100

    fix: add documentation comments for Friends component to improve clarity

--- src/componenents/user/friends-related/Friends.tsx
+++ src/componenents/user/friends-related/Friends.tsx
@@ -6,6 +6,28 @@ export const FriendComponentContext = React.createContext<{
   toggleOnOutlet: React.Dispatch<React.SetStateAction<boolean>>;
 } | null>(null);
 
+/**
+ * Friends component that manages the display of either the `Outlet` or `FriendList` component
+ * based on the `isOutLet` state.
+ *
+ * @component
+ * @example
+ * return (
+ *   <Friends />
+ * )
+ *
+ * @returns {JSX.Element} The rendered Friends component.
+ *
+ * @remarks
+ * This component uses the `FriendComponentContext.Provider` to provide a context value
+ * that includes a function to toggle the `isOutLet` state.
+ *
+ * @hook
+ * - `useState<boolean>`: Manages the `isOutLet` state to determine which component to display.
+ *
+ * @context
+ * - `FriendComponentContext.Provider`: Provides the `toggleOnOutlet` function to child components.
+ */
 const Friends = () => {
   const [isOutLet, SetisOutLet] = useState<boolean>(false);
 
