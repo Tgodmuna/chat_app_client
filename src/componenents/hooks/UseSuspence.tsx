6115e0e2d548956872b4d0ecf486cdc5a88233c1
Aguthankgod <Aguthankgod@gmail.com>
2025-03-04 16:30:13 +0100

    fix: update loading message in UseSuspence component

--- src/componenents/hooks/UseSuspence.tsx
+++ src/componenents/hooks/UseSuspence.tsx
@@ -1,9 +1,5 @@
 import React, { Suspense } from "react";
 
 export const UseSuspence: React.FC<{ component: React.JSX.Element }> = ({ component }) => {
-    return (
-        <Suspense fallback={<div>Loading...</div>}>
-            {component}
-        </Suspense>
-    );
+  return <Suspense fallback={<div>lazy Loading...</div>}>{component}</Suspense>;
 };
